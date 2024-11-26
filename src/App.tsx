2161a134052ca8ba13db8a307ff7c0b002cfe236
ghadeer kherullah <kherullahghadeer@gmx.com>
2024-11-26 17:38:13 +0100

    add a counterand total price to the cart

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import AppContent from "./components/AppContent";
 import AppFooter from "./components/AppFooter";
 import AppHeader from "./components/AppHeader";
 import { getProduct } from "./services/Products";
+import { useState } from "react";
 
 const products = [...Array(10)].map(() => getProduct());
 
 function App() {
+  const [cart, setCart] = useState([]);
   return (
     <Layout>
       <BrowserRouter>
-        <AppHeader />
-        <AppContent productsData={products} />
+        <AppHeader cart={cart} />
+        <AppContent productsData={products} cart={cart} setCart={setCart} />
         <AppFooter />
       </BrowserRouter>
     </Layout>
