9616556cf9a7e69413af53a8f305a118cb763f7a
ghadeer kherullah <kherullahghadeer@gmx.com>
2024-11-26 16:43:56 +0100

    display products on product page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
 import { Layout } from "antd";
 import { BrowserRouter } from "react-router-dom";
-
 import AppContent from "./components/AppContent";
 import AppFooter from "./components/AppFooter";
 import AppHeader from "./components/AppHeader";
-
 import { getProduct } from "./services/Products";
 
 const products = [...Array(10)].map(() => getProduct());
-
-console.log(products);
 
 function App() {
   return (
     <Layout>
       <BrowserRouter>
         <AppHeader />
-        <AppContent />
+        <AppContent productsData={products} />
         <AppFooter />
       </BrowserRouter>
     </Layout>
